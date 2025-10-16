ea6e7d91fb70eaa5a66bd7ce782a72f8269fd1b7
qr.ye <qr.ye@106-11.info2>
2025-10-16 14:39:20 +0200

    Falta por terminar

---
+++ src/boletín/Ejercicio01.java
@@ -0,0 +1,31 @@
+package boletín;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		
+		// Crear Scanner
+		Scanner reader = new Scanner(System.in); 
+		
+		// Crear variables
+		int num; 
+		
+		// Pedir al usuario un número
+		System.out.println("Introduce un número: ");
+		num = reader.nextInt(); 
+		
+		// Crear condicional para mostrar en pantalla si el número introducido por el usuario es par o impar
+		if (num%2 == 0) { 
+			System.out.println("Par ");
+		} else { 
+			System.out.println("Impar ");
+		}
+		
+		// Cerrar Scanner
+		reader.close();
+
+	}
+
+}
---
+++ src/boletín/Ejercicio03.java
@@ -0,0 +1,10 @@
+package boletín;
+
+public class Ejercicio03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
