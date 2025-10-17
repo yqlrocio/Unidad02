48958149ff66b40bb1dd32458545ae262cb53a68
qr.ye <qr.ye@106-11.info2>
2025-10-17 13:42:07 +0200

    SIN TERMINAR

--- src/boletín/Ejercicio03.java
+++ src/boletín/Ejercicio03.java
@@ -1,9 +1,51 @@
 package boletín;
 
+import java.util.Scanner;
+
 public class Ejercicio03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Crear Scanner
+		Scanner reader = new Scanner(System.in); 
+		
+		// Crear variables
+		int a; 
+		int b; 
+		int c; 
+		
+		double x1; 
+		double x2;
+		
+		double discriminante; 
+		
+		// Pedir los tres coeficientes al usuario para resolver posteriormente la ecuación de segundo grado
+		System.out.println("Introduzca los coeficientes de la ecuación de segundo grado: ");
+		a = reader.nextInt(); 
+		b = reader.nextInt(); 
+		c = reader.nextInt(); 
+		
+		discriminante = b*b - 4*a*c; 
+		
+		// Si a == 0 es de primer grado
+		if (a ==0 ) {
+			x1 = (double) - c/ b; 
+			System.out.println("La ecuación tiene solo una solución: " + x1);
+		} else if (discriminante >= 0) {
+			
+			// Si es positivo, tiene solución y se aplica la fórmula
+			x1 = (-b + Math.sqrt(discriminante)) / (2*a); 
+			x2 = (-b - Math.sqrt(discriminante)) / (2*a);
+			System.out.println("x1: " + x1);
+			System.out.println("x2: " + x2);
+		} else {
+			
+			// Si es negativo no tiene solución 
+			System.out.println("La ecuación no tiene solución: ");
+		}
+		
+		// Cerrar Scaner 
+		reader.close(); 
 
 	}
 
--- src/boletín/Ejercicio05.java
+++ src/boletín/Ejercicio05.java
@@ -1,10 +1,54 @@
 package boletín;
 
+import java.util.Scanner;
+
 public class Ejercicio05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+		 //Create a Scanner
+        Scanner sc = new Scanner (System.in);
+        
+        //The movement of the first player
+        String firstPlayer;
+        
+        //The movement of the second player
+        String secondPlayer;
+        
+        //The movement 'Piedra'
+        final String PIEDRA = null;
+        
+        //The movement 'Papel'
+        final String PAPEL = null;
+        
+        //The movement 'Tijeras'
+        final String TIJERAS = null;
+        
+        //The console waits for the player to tell what they're going to do
+        System.out.println("Elija piedra, papel o tijeras:");
+        firstPlayer = sc.nextLine();
+        
+        System.out.println("El otro jugador contraataca con:");
+        secondPlayer = sc.nextLine();
+        
+        //We establish how one the users would win
+        if (!firstPlayer.equals(TIJERAS) || !firstPlayer.equals(PAPEL) || !firstPlayer.equals(PIEDRA)) {
+            System.out.println("Primera jugada incorrecta");
+        } else if (!firstPlayer.equals(TIJERAS) && !firstPlayer.equals(PAPEL) && !firstPlayer.equals(PIEDRA)) {
+            System.out.println("Segunda jugada incorrecta");
+        } else if (firstPlayer.equals(secondPlayer)) {
+            System.out.println("EMPATE");
+        } else if (firstPlayer.equals(PIEDRA) && secondPlayer.equals(TIJERAS)
+                || firstPlayer.equals(PAPEL) && secondPlayer.equals(PIEDRA)
+                || firstPlayer.equals(TIJERAS) && secondPlayer.equals(PAPEL)) {
+            System.out.println("Gana el primer jugador");
+        } else {
+            System.out.println("Gana el segundo jugador");
+        }
+        
+        //Close Scanner
+        sc.close();
+        
+    }
 
 }
