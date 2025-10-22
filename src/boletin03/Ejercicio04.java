b314b29b0c04547323125238a38348b8857a5e4c
qr.ye <qr.ye@106-10.info2>
2025-10-22 10:26:01 +0200

    Finalizado

--- src/boletin03/Ejercicio04.java
+++ src/boletin03/Ejercicio04.java
@@ -1,5 +1,6 @@
 package boletin03;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio04 {
@@ -7,16 +8,39 @@ public class Ejercicio04 {
 	public static void main(String[] args) {
 		
 		// Crear Scanner 
-		Scanner reader = new Scanner(System.in);
+		Scanner reader1 = new Scanner(System.in); 
+		// Crear Ramdon
+		Random reader2 = new Random(); 
 		
 		// Crear variables
+		int num1 = reader2.nextInt(1, 100); 
+		int num2 = reader2.nextInt(1, 100); 
+		int resultadoReal; 
+		int resultadoUsuario; 
 		
+		// Enseñarle al usuario 2 números aleatorios
+		System.out.println(num1);
+		num1 = reader2.nextInt();
 		
-		// Pedir al usuario que introduzca 2 números
+		System.out.println(num2);
+		num2 = reader2.nextInt();
 		
-		// Cerrar Scanner 
-		reader.close(); 
+		// Hacer operacion de los valores aleatorios
+		resultadoReal = num1 + num2; 
+		
+		// Pedir al usuario que realice la suma de los 2 números mostrados en pantalla. Finalmente, el programa usando condicionales if-else mostrará en pantalla si el resultado del usuario es correcto o no
+		System.out.println("Realiza la suma de los valores anteriores e introducelo: ");
+		resultadoUsuario = reader1.nextInt(); 
+		
+		// Crear condición para comrpobar si el resultado introducido por el usurio es correcto 
+		if (resultadoUsuario == resultadoReal) {
+			System.out.println("El resultado es correcto");
+		} else {
+			System.out.println("El resultado es incorrecto");
+		}
 
+		// Cerrar Scanner 
+		reader1.close(); 
+		}
 	}
 
-}
