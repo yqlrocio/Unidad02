32808f0f5fd809540440f82736f7f9c940b5dad9
qr.ye <qr.ye@106-27.info2>
2025-10-31 12:39:32 +0100

    sin terminar

--- src/boletin04/Ejercicio05.java
+++ src/boletin04/Ejercicio05.java
@@ -6,7 +6,41 @@ public class Ejercicio05 {
 
 	public static void main(String[] args) {
 	
-		
-	}		
+		//Create a Scanner
+        Scanner sc = new Scanner (System.in);
+        
+        //The sum of the number
+        int sumaAlumnos = 0; 
+        int numAlumnos = 0; 
+        int numMayoresEdad = 0;
+        double media;
+        
+        //The numbers introduced by the user
+        int edad;
+        
+        //The console waits for the user to introduce a number
+        System.out.println("Introduzca una edad:");
+        edad = sc.nextInt();
+        
+        //We'll only sum the positive numbers, and stop when the user introduces a negative one
+        while (edad>=0) {
+          sumaAlumnos += edad;
+          numAlumnos ++; 
+            System.out.println("Siga introduciendo número:");
+            edad = sc.nextInt();
+        } 
+       while (edad >= 18) {
+    	   numMayoresEdad ++: 
+    	 System.out.println("Siga introduciendo número:");
+           edad = sc.nextInt();
+       }
+        media = (double) sumaAlumnos / numAlumnos; 
+        
+        //We will show the final result of the sum after the user has introduced a negative number
+        System.out.println("La media de los números introducido es: " + media);
+        
+        //Close Scanner
+        sc.close();
+
+    }
 }
-	
