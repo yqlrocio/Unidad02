8978dd596b45b01b565f15cf97d4604b5e93486a
qr.ye <qr.ye@106-27.info2>
2025-11-06 14:35:27 +0100

    sin terminar

--- src/boletin05/Ejercicio08.java
+++ src/boletin05/Ejercicio08.java
@@ -1,9 +1,35 @@
 package boletin05;
 
+import java.util.Scanner;
+
 public class Ejercicio08 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Craer Scanner 
+		Scanner reader = new Scanner(System.in); 
+		
+		// Crear variable para almacenar el valor introducido por el usuario 
+		int num1; 
+		int num2;
+		
+		String tirada1;
+		String tirada2; 
+
+		switch (tirada1) {
+		case "UNO" -> num1 = 1; 
+		case "DOS" -> num1 = 2; 
+		case "TRES" -> num1 = 3; 
+		case "CUATRO" -> num1 = 4; 
+		case "CINCO" -> num1 = 5; 
+		case "SEIS" -> num1 = 6; 
+		DEFAULT -> {
+			System.out.println("Tirada errónea");
+			tirada2 = -1
+		}
+		
+		// Cerrar Scanner 
+		reader.close(); 
 
 	}
 
---
+++ src/boletin06/Ejercicio01.java
@@ -0,0 +1,29 @@
+package boletin06;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+	public static void main(String[] args) {
+		
+		// Crear Scanner 
+		Scanner reader = new Scanner(System.in);
+		
+		// Crear vriables
+		int num; 
+		
+		// Le pedimos un número al usuario
+		System.out.println("Introduzca un número");
+		num = reader.nextInt(); 
+		
+		// For: desde 1 hasta num de 1 en 1
+		for (int cont = 1; cont<=num; cont++) {
+			System.out.println(cont);
+		}
+		
+		// Cerrar Scanner 
+		reader.close();
+
+	}
+
+}
